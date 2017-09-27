c21a901d62436960367d91217b4fcc2596f5472f
AmsterGet <amsterget@gmail.com>
2017-09-27 21:16:10 +0300

    Easy task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.l=min;
+        this.r=max;
     }
 
     guess() {
-
+        return Math.round((this.l+this.r)/2);
     }
 
     lower() {
-
+        this.r=Math.round((this.l+this.r)/2);
     }
 
     greater() {
-
+        this.l=Math.round((this.l+this.r)/2);
     }
 }
 
